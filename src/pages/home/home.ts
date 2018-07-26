db63eafe90a1a0be9cadbb9d45bec6cbe9ef6ccc
Lahar <shahlahar@outlook.com>
2018-07-26 14:19:00 -0500

    Exercise 1 shop and buyout page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {UsersPage} from "../users/users";
+import {ShopPage} from "../shop/shop";
 
 @Component({
     selector: 'page-home',
@@ -14,5 +15,9 @@ export class HomePage {
 
     goToUsers() {
         this.navCtrl.push(UsersPage);
+    }
+
+    goToShop() {
+        this.navCtrl.push(ShopPage);
     }
 }
